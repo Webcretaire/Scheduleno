14b5bb6e24bf23bcc603ef523bd680327ba281e5
Julien EMMANUEL <julien.emmanuel@inria.fr>
2021-01-13 11:54:30 +0100

    Simplify main script
    Put help and bad usage functions in util, so that the main script is as
    simple as possible

--- scheduleno.ts
+++ scheduleno.ts
@@ -1,6 +1,6 @@
 import { Args, parse } from "https://deno.land/std@0.79.0/flags/mod.ts";
-import { bold, red } from "https://deno.land/std@0.79.0/fmt/colors.ts";
 import { Session } from "./scheduler.ts";
+import { printBadUsage, printHelp } from "./util.ts";
 
 const args: Args = parse(Deno.args, {
   alias: {
@@ -17,53 +17,14 @@ const args: Args = parse(Deno.args, {
   },
 });
 
-const usageStr = `${bold("Usage:")} scheduleno [OPTIONS] FILENAME`;
-
 if (args.help) {
-  console.log(
-    "This utility helps schedule jobs efficiently on a multi-core machine",
-  );
-  console.log(" ");
-  console.log(usageStr);
-  console.log(" ");
-  console.log(
-    bold("FILENAME"),
-    "is the path to a text file containing one commad per line, to be executed by workers",
-  );
-  console.log(" ");
-  console.log(bold("OPTIONS"), " can include :");
-  console.log(" ");
-  console.log(
-    "  ",
-    bold("--parallel-workers (-p) :"),
-    "Request a specific number of workers. If unspecified, the scheduler will try to find the optimal number based on the number of cores on the machine",
-  );
-  console.log(" ");
-  console.log(
-    "  ",
-    bold("--timeout (-t) :"),
-    "Timeout for each job, the expected format is similar to GNU's timeout utility. Defaults to 1 day (1d)",
-  );
-  console.log(" ");
-  console.log(
-    "  ",
-    bold("--help (-h) :"),
-    "Display this help message and exit",
-  );
-
+  printHelp();
   Deno.exit(0);
 }
 
 // Make sure we have a list of commands to run
 if (args._.length != 1) {
-  console.error(
-    red(bold("Error:")),
-    "This script requires a filename (command file)",
-  );
-  console.error(usageStr);
-  console.error(
-    "For more details run `./scheduleno --help`",
-  );
+  printBadUsage();
   Deno.exit(1);
 }
 
--- util.ts
+++ util.ts
@@ -3,13 +3,14 @@ import {
   IExecResponse,
   OutputMode,
 } from "https://deno.land/x/exec/mod.ts";
+import { bold, red } from "https://deno.land/std@0.79.0/fmt/colors.ts";
 
 /**
  * Choose a number of workers based on the numbers of cores on the machine
  */
-export function chooseNumberOfWorkers(): Promise<number> {
+export const chooseNumberOfWorkers = (): Promise<number> =>
   // Get number of cores on this machine
-  return exec(
+  exec(
     "grep -c processor /proc/cpuinfo",
     { output: OutputMode.Capture },
   ).then((systemResponse: IExecResponse) =>
@@ -21,4 +22,49 @@ export function chooseNumberOfWorkers(): Promise<number> {
       1,
     )
   );
-}
+
+const usageStr = `${bold("Usage:")} scheduleno [OPTIONS] FILENAME`;
+
+export const printHelp = () => {
+  console.log(
+    "This utility helps schedule jobs efficiently on a multi-core machine",
+  );
+  console.log("");
+  console.log(usageStr);
+  console.log("");
+  console.log(
+    bold("FILENAME"),
+    "is the path to a text file containing one commad per line, to be executed by workers",
+  );
+  console.log("");
+  console.log(bold("OPTIONS"), " can include :");
+  console.log("");
+  console.log(
+    "  ",
+    bold("--parallel-workers (-p) :"),
+    "Request a specific number of workers. If unspecified, the scheduler will try to find the optimal number based on the number of cores on the machine",
+  );
+  console.log("");
+  console.log(
+    "  ",
+    bold("--timeout (-t) :"),
+    "Timeout for each job, the expected format is similar to GNU's timeout utility. Defaults to 1 day (1d)",
+  );
+  console.log("");
+  console.log(
+    "  ",
+    bold("--help (-h) :"),
+    "Display this help message and exit",
+  );
+};
+
+export const printBadUsage = () => {
+  console.error(
+    red(bold("Error:")),
+    "This script requires a filename (command file)",
+  );
+  console.error(usageStr);
+  console.error(
+    "For more details run `./scheduleno --help`",
+  );
+};
